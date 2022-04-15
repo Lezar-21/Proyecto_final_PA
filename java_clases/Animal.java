94395a626c2b51e5b3e976c48616437e9f21b1cf
David Vega Tepetla <davidvega.cb165a@gmail.com>
2022-04-15 15:44:23 -0500

    Se modifico el nombre de la clase Articulo por animal

---
+++ java_clases/Animal.java
@@ -0,0 +1,35 @@
+package Proyecto_Final.java_clases;
+
+import Proyecto_Final.Ventanas.MiPanel;
+import java.awt.Graphics2D;
+
+//la clase "Articulo" funciona como nodo para la cola llamada "Monitor" 
+public class Animal {
+    int p;//prueba
+
+    int x = 0, y = 0,  velX, velY, tam;    
+    MiPanel mp;
+
+    //prueba
+    public Articulo(int valor){
+        this.p = valor;
+    }
+
+    public int getDato(){
+        return p;
+    }
+
+    public Articulo(MiPanel mpx, int xx, int xy, int xvel, int xtam){
+        this.mp = mpx;
+        this.x = xx;
+        this.y = xy;
+        this.velX = xvel;
+        this.velY = xvel;
+        this.tam = xtam;
+    }
+    public void dibujarArticulo(Graphics2D g2){
+
+        g2.fillOval(x,y,tam,tam);
+
+      }
+}
